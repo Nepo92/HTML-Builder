5624acfd3aac8ea6d071f33271c606a8d94d88e8
Alexandr Eremeev <eremeev.a.e@mail.ru>
2022-11-05 17:29:53 +0700

    feat: task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const pathFile = path.resolve(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(pathFile, 'utf-8');
+
+writeStream.on('open', () => {
+    console.log('Hello, please type a text in file:');
+});
+
+rl.on('close', () => {
+    writeStream.close();
+});
+
+writeStream.on('close', () => {
+    console.log('bye');
+    rl.close();
+});
+
+rl.on('line', (data) => {
+    if (data.toLowerCase() === 'exit') {
+        writeStream.close();
+    } else {
+        writeStream.write(data);
+    }
+});
